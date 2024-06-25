72f83a9fbc9b786be63809fd09bd32ee2646a0ec
Noam Almog <noamza@gmail.com>
2024-06-25 16:58:21 +0200

    making ds good

--- ds.m
+++ ds.m
@@ -1,3 +1,15 @@
 function d = ds(a)
-    d=datastats(toCol(a));
+    d=struct();
+    d.size = size(a);
+    d.mean = mean(a(:));
+    d.min = min(a(:));
+    d.max = max(a(:));
+    d.std = std(a(:));
+    d.median = median(a(:));
+    d.mode = mode(a(:));
+    if len(a) < 1e4 && len(unique(a(:))) < 30
+        d.unique = torow(unique(a(:)));
+    end
 end
+
+%d=datastats(toCol(a));
