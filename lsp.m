6d4139107bc92f5cd82c5ac342f905877cdba6d5
Noam <noamza@gmail.com>
2024-06-29 12:06:51 +0200

    linspace without last point

--- lsp.m
+++ lsp.m
@@ -1,3 +1,9 @@
 function l = lsp(varargin)
-    l = linspace(varargin{:});
+    p={varargin{:}};
+    if len(p)==3
+        l = linspace(p{:});
+    else
+        l = linspace(p{1:3});
+        l = l(1:end-1);
+    end
 end
