475ee24f9a7af33ad8e18c9dedb5946fbb6e506d
Noam Almog <noamza@gmail.com>
2024-07-01 22:50:22 +0200

    better commit

--- commit.m
+++ commit.m
@@ -1,12 +1,19 @@
 function commit(m,push)
     
+    disp('adding: ')
     !git add .
-    
+
+    disp('committing: ')
     eval("!git commit -a  -m " + """" + m + """" ); %adds quotest around commit string ""=" in strings
 
     if exist("push","var")
+
+        disp('pulling: ')
         !git pull
+        
+        disp('pushing: ')
         !git push
+
     end
 end
 
