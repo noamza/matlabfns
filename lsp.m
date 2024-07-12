e213aad2d1a53cd1bbb39e2027158c39b9aaeb0e
Noam Almog <noamza@gmail.com>
2024-07-12 21:15:29 +0200

    before the vacay

--- lsp.m
+++ lsp.m
@@ -3,7 +3,7 @@ function l = lsp(varargin)
     if len(p)==3
         l = linspace(p{:});
     else
-        l = linspace(p{1:3});
+        l = linspace(p{1:2},p{3}+1);
         l = l(1:end-1);
     end
 end
